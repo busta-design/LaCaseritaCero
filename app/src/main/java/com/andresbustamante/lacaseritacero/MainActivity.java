6d54d533887a0a4d253b6c9374a83ad279930374
Andres <andresbusta4444@gmail.com>
2020-06-24 08:30:54 -0400

    RecyclerView with simple lists of image, quatity and purchase item

--- app/src/main/java/com/andresbustamante/lacaseritacero/MainActivity.java
+++ app/src/main/java/com/andresbustamante/lacaseritacero/MainActivity.java
@@ -1,21 +1,81 @@
 package com.andresbustamante.lacaseritacero;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ClipData;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements RecyclerAdapter.ListItemClick{
+
+    private RecyclerView mainRecyclerView;
+    ArrayList<String> listaDatos;
+    //private static final int LISTA_NUMEROS = 100;
+    private Toast toast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         setTheme(R.style.AppTheme); //cambio del splashscreen
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //todo IMPLEMENTAR UN RECYCLERVIEW DONDE MUESTRE UNA LISTA INFINITAss
-        //dsdsds
-        //dsds
+        mainRecyclerView = findViewById(R.id.mainrecyclerview);
+        mainRecyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL)); //es la linea de division
+        mainRecyclerView.setLayoutManager(new LinearLayoutManager(this));// uno de los tres Linear, Grid, StaggeredGrid
+        //mainRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
+        listaDatos = new ArrayList<String>();
+        llenarListaDatos();
+
+
+        //llamamos al adaptador
+        RecyclerAdapter adapter = new RecyclerAdapter(listaDatos, this);
+        mainRecyclerView.setAdapter(adapter);
+
+        //debemos agregar la imagen a medias
+
+
+
     }
+
+    @Override
+    public void onListItemClick(int clickedItem) {
+        if (toast != null) {
+            toast.cancel();
+        }
+        String mensajeToast = "item #"+ clickedItem + " clickado";
+        toast = Toast.makeText(this, mensajeToast, Toast.LENGTH_SHORT);
+        toast.show();
+
+
+    }
+
+    public void llenarListaDatos() {
+        listaDatos.add("Carne");
+        listaDatos.add("Pollo");
+        listaDatos.add("Tomate");
+        listaDatos.add("Lechuga");
+        listaDatos.add("Apio");
+        listaDatos.add("Chorizo");
+        listaDatos.add("Veterraga");
+        listaDatos.add("Fideo");
+        listaDatos.add("Cerdo");
+        listaDatos.add("Papa");
+        listaDatos.add("Arberja");
+        listaDatos.add("Salchicha");
+        listaDatos.add("Hamburguesa");
+        listaDatos.add("Chuleta");
+        listaDatos.add("Costilla");
+        listaDatos.add("Morron");
+        listaDatos.add("Frijol ");
+
+
+    }
+
 }
---
+++ app/src/main/java/com/andresbustamante/lacaseritacero/RecyclerAdapter.java
@@ -0,0 +1,94 @@
+package com.andresbustamante.lacaseritacero;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.NumerosViewHolder>{
+    ArrayList<String> listaDatos;
+    //private int mainNumeroItems;
+    private final ListItemClick listItemClick;
+
+    public RecyclerAdapter(ArrayList<String> listaDatos, ListItemClick listItemClick) {
+        this.listaDatos = listaDatos;
+        this.listItemClick = listItemClick;
+    }
+
+    //para poder ponerle clicks a lis elementos
+    public interface ListItemClick {
+        void onListItemClick(int clickedItem);
+    }
+
+
+    @NonNull
+    @Override
+    public NumerosViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //inflar la vista de lo que creamos en el 'lista_numeros_raw.xml'
+        //rellenar cada elemento del recyclerview con vista
+
+        Context context = parent.getContext();
+        int layoutIdParaListItem = R.layout.lista_numeros_raw; //los ID son NUMEROS, nos guardaremos el del layout
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean atachParentRapido = false; //facilitar
+
+        View view = inflater.inflate(layoutIdParaListItem, parent, atachParentRapido);
+
+        //nos pasamos nuestra propia vista de la clase anidada
+
+        NumerosViewHolder viewHolder = new NumerosViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NumerosViewHolder holder, int position) {
+        //Bindear (bind=comportamiento) a cada vista del ViewHolder con los datos
+        holder.bind(listaDatos.get(position));
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaDatos.size(); //devuelve la cantidad de elementos que hay en el RecyclerView / nos llega desde el constructor / no es .length()
+    }
+
+
+    class NumerosViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        TextView textViewListaNumeros;
+        TextView textViewCantidad;
+        ImageView imageView;
+
+        public NumerosViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            textViewListaNumeros = itemView.findViewById(R.id.textview_listanumeros);
+            textViewCantidad = itemView.findViewById(R.id.cantidad);
+            imageView = itemView.findViewById(R.id.imagen);
+
+            itemView.setOnClickListener(this);
+        }
+
+        //onBindViewHolder es para que todas las vistas sean iguales, les pasa la misma vista a todos
+        void bind(String dato) {
+            textViewListaNumeros.setText(dato + "\t\t\t\t\t\t\t");
+            //aprender mas sobre linear layout
+
+        }
+
+        @Override
+        public void onClick(View v) {
+            int clickedItem = getAdapterPosition(); // obtenemos la posicion del click llamado
+            listItemClick.onListItemClick(clickedItem);
+        }
+    }
+}
